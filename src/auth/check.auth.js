c1d47f863a4441030a8594113d01ee46114eb5df
baoacer <nguyenquocbaotu@gmail.com>
2024-12-04 08:06:07 +0700

    convert asyncHandler to separate file

--- src/auth/check.auth.js
+++ src/auth/check.auth.js
@@ -53,13 +53,7 @@ class CheckAuth {
             }
         };
     };
-
-    static asyncHandler = asyncFunc => {
-        return (req, res, next) => {
-          asyncFunc(req, res, next).catch(next)
-        }
-      }
-      
+    
 }
 
 module.exports = CheckAuth
