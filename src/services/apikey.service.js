56864c5b325d3b098e73aff8d0696b91510b8b22
baoacer <nguyenquocbaotu@gmail.com>
2024-12-09 05:39:50 +0700

    findById

--- src/services/apikey.service.js
+++ src/services/apikey.service.js
@@ -1,6 +1,6 @@
 "use strict";
 const ApiKeyModel = require('../models/apikey.model')
-
+const crypto = require('crypto')
 class ApikeyService{
     static findById = async (key) => {
         const objKey = await ApiKeyModel.findOne({key: key, status: true}).lean();
