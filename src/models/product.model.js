56a25e0c79286b174afc7c472794814cc5b6755f
baoacer <nguyenquocbaotu@gmail.com>
2024-12-09 05:41:53 +0700

    update product.model add slug, rating, variations, isDraft, isPublished

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -18,12 +18,12 @@ const ProductSchema = new Schema(
     product_type: { type: String, required: true, enum: ["Electronic", "Clothing", "Furniture"] },
     product_shop: { type: Schema.Types.ObjectId, ref: "Shop" },
     product_attributes: { type: Schema.Types.Mixed, required: true },
-    product_ratingsAverage: { type: Number, default: 4.5, 
+    product_ratingsAverage: { type: Number, default: 0.0, 
       min: [1, 'Rating must be above 1.0'],
       max: [5, 'Rating must be below 5.0'],
-      set: (val) => Math.round(val * 10) / 10  //set: setter biến đổi value trước khi save to db
+      set: (val) => Math.round(val * 10) / 10  //set: biến đổi value trước khi save to db
     },
-    product_variations: { type: Array, default: [] },
+    product_variations: { type: Array, default: [] }, // ex: color : price
     isDraft: { type: Boolean, default: true, index: true, select: false },
     isPublished: { type: Boolean, default: false, index: true, select: false },
   },
