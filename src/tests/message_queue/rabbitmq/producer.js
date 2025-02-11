a8ab103f9dc2dbf2747495db89ca7d520fc59405
baoacer <nguyenquocbaotu@gmail.com>
2025-02-11 21:47:15 +0700

    test rabbitmq

--- src/tests/message_queue/rabbitmq/producer.js
+++ src/tests/message_queue/rabbitmq/producer.js
@@ -3,14 +3,20 @@ const messages = 'Hello RabbitMQ user!';
 
 const runProducer = async () => {
     try {
-        const connection = amqp.connect('amqp://localhost')
-        const chanel = await connection.createChannel()
+        const connection = await amqp.connect('amqp://guest:root@localhost')
+        const channel = await connection.createChannel()
 
         const queueName = 'test-topic'
-        await chanel.assertQueue(queueName, { 
+        await channel.assertQueue(queueName, { 
             durable: true // if true, the queue will be saved when the server restarts 
          })
+
+         await channel.sendToQueue(queueName, Buffer.from(messages))
+
+         console.log(`[Producer] Message sent: ${messages}`)
     } catch (error) {
-        
+        console.error(error)
     }
 }
+
+runProducer().catch(console.error)
